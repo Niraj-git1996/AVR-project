6982b97ed7de6b57ec527e71a45ec7fe4e7335f1
Niraj-git1996 <contact.niraj1996@gmail.com>
2020-06-02 17:23:19 +0530

    buzzer and led with different switch

--- Pushbutton_with_Led/main.c
+++ Pushbutton_with_Led/main.c
@@ -9,19 +9,29 @@
 
 int main(void)
 {
-   DDRB |=(1<<DDB2)|(1<<DDB3)|(1<<DDB4);   /// making portB pin 2 and 3 as output and led is connected
-   DDRB &=~(1<<DDB0);            /// making portB pin 0 will be input pushbutton is connected
+   DDRB |=(1<<DDB2)|(1<<DDB3)|(1<<DDB4);   /// making portB pin 2,3 and 4 as output and led is connected
+   DDRB &=~(1<<DDB0)&(~(1<<DDB1));            /// making portB pin 0 and 1 will be input pushbutton is connected
 
     while(1)
     {
     if (!(PINB&(1<<PINB0)))     ///this pushbutton active low when pressing the push button led will glow
 		{
-		PORTB |= (1<<PINB2)|(1<<PINB3)|(1<<PINB4);   ///pin 4 has buzzer connected with it.
+		PORTB |= (1<<PINB2)|(1<<PINB3);
 		}
 		else
 		{
-		PORTB &= (~(1<<PINB2)) &(~(1<<PINB3))&(~(1<<PINB4));   ///led will off and buzzer will off
+		PORTB &= (~(1<<PINB2)) &(~(1<<PINB3));   ///led will off
 		}
+     if(!(PINB&(1<<PINB1)))
+     {
+
+         PORTB |=(1<<PINB4);             ///pin 4 has buzzer connected with it.
+     }
+     else
+     {
+
+         PORTB&=(~(1<<PINB4));
+     }
     }
 
     return 0;
