689c07d39c6ec2ae44e6f9709d6bad0311b44f3b
Niraj-git1996 <contact.niraj1996@gmail.com>
2020-06-02 16:50:59 +0530

    push button with led

---
+++ Pushbutton_with_Led/README.txt
@@ -0,0 +1,2 @@
+The code contain push button control the led with AVR.
+The above code is verified with picsimlab emulator.Also added the emulator files .pzw to execute the same
---
+++ Pushbutton_with_Led/main.c
@@ -0,0 +1,28 @@
+/*
+  The code contain push button control the led with AVR
+  author:Niraj
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 1000000UL
+
+int main(void)
+{
+   DDRB |=(1<<DDB2)|(1<<DDB3);   /// making portB pin 2 and 3 as output and led is connected
+   DDRB &=~(1<<DDB0);            /// making portB pin 0 will be input pushbutton is connected
+
+    while(1)
+    {
+    if (!(PINB&(1<<PINB0)))     ///this pushbutton active low when pressing the push button led will glow
+		{
+		PORTB |= (1<<PINB2)|(1<<PINB3);
+		}
+		else
+		{
+		PORTB &= (~(1<<PINB2)) &(~(1<<PINB3));   ///led will off
+		}
+    }
+
+    return 0;
+}
