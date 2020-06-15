588fb37c4b3cd7d23162ca6d061acfbc3c18fd2b
Niraj-git1996 <contact.niraj1996@gmail.com>
2020-06-16 00:41:55 +0530

    Updated  with Pin change Interrupts code

--- Ext_Int_prog/Pin_change_INT1.c
+++ Ext_Int_prog/Pin_change_INT1.c
@@ -3,7 +3,7 @@ in atmega 328p.there are 22 PCINTx pin .The PCICR are used to enable or disable 
     PCIE1(PCINT8-PCINT14)  and PCIE2(PCINT16-PCINT23)  . the particular Pin can be mask PCMSK0(PCINT0-PCINT7)
     PCMSK1(PCINT8-PCINT14)  and PCMSK2(PCINT16-PCINT23)
 */
-/// In this program we used PCIE0 to set and enable PCMSK0
+///  Tn this code the PCIE1 bit is set in PCICR and PCIMSK1 is enable pin change interrupt from PC0-PC6
 #include <avr/io.h>
 #include<avr/interrupt.h>    /// include interrupt header file
 #include <util/delay.h>
@@ -12,11 +12,11 @@ in atmega 328p.there are 22 PCINTx pin .The PCICR are used to enable or disable 
 #define F_CPU  16000000UL
 #endif // F_CPU
 
-#define Int_0  (1<<PINB0)    /// PCINT0
-#define  Int_1  (1<<PINB1)    /// PCINT1
+#define Int_8  (1<<PINC0)    /// PCINT8
+#define  Int_9  (1<<PINC1)    /// PCINT9
 
-#define  led_1 (1<<PIND0)   /// OUTPUT CONTROL BY PCINT0
-#define  buzz   (1<<PIND1)    ///OUTPUT CONTROL BY PCINT1
+#define  led_1 (1<<PIND0)   /// OUTPUT CONTROL BY PCINT8
+#define  buzz   (1<<PIND1)    ///OUTPUT CONTROL BY PCINT9
 
 void TimerInit()
 {
@@ -27,34 +27,34 @@ void TimerInit()
 }
 int main(void)
 {
-    DDRB &= (~Int_0)&(~Int_1);     // Clear the PB0 AND PB1pin
-    // PB0 (PCINT0 pin) is now an input
+    DDRB &= (~Int_8)&(~Int_9);     // Clear the PC0 AND PC1pin
+    // PC0 AND PC1  is now an input
     DDRD|=led_1|buzz;
-    PORTB |= Int_0|Int_1;    // turn On the Pull-up
-    // PB0 AND PB1is now an input with pull-up enabled
+    PORTC |= Int_8|Int_9;    // turn On the Pull-up
+    // PC0 AND PC1 is now an input with pull-up enabled
      //TimerInit();
 
-    PCICR |= (1 << PCIE0);    // set PCIE0 to enable PCMSK0 scan
-    PCMSK0 |= (1 << PCINT0)|(1<<PCINT1);  // set PCINT0 AND PCINT1 to trigger an interrupt on state change
+    PCICR |= (1 << PCIE1);    // set PCIE1 to enable PCMSK1 scan
+    PCMSK1 |= (1 << PCINT8)|(1<<PCINT9);  // set PCINT8 AND PCINT9 to trigger an interrupt on state change
     //TIMSK0=(1<<OCIE0A);
-    sei();
+    sei();   /// SET THE GLOBAL INTERRUPT ENABLE
     //sei();
     while(1);
     {
-
+   /// SIMULTANEOUSLY CAN DO MANY TASK
 
     }
     return 0;
 }
-ISR (PCINT0_vect)
+ISR (PCINT1_vect)
 {
-    if(!(PINB&Int_0))    /// ACTIVE LOW
+    if(!(PINC&Int_8))    /// ACTIVE LOW
     {
        PORTD|=led_1;
     }
     else
         PORTD&=(~led_1);
-    if(!(PINB&Int_1))    ///ACTIVE LOW
+    if(!(PINC&Int_9))    ///ACTIVE LOW
     {
         PORTD|=buzz;
     }
@@ -68,3 +68,4 @@ ISR (PCINT0_vect)
 //        PORTD&=(~buzz);
     /* interrupt code here */
 }
+S
--- Ext_Int_prog/main.c
+++ Ext_Int_prog/main.c
@@ -1,9 +1,10 @@
-/*in this program the Pin change interrupt is used to perform different operation .There are predefined pin change interrupt Pin
+/*The Pin change interrupt is used to perform different operation .There are predefined pin change interrupt Pin
 in atmega 328p.there are 22 PCINTx pin .The PCICR are used to enable or disable interrupt PCIE0(PCINT0-PCINT7)
     PCIE1(PCINT8-PCINT14)  and PCIE2(PCINT16-PCINT23)  . the particular Pin can be mask PCMSK0(PCINT0-PCINT7)
-    PCMSK1(PCINT8-PCINT14)  and PCMSK2(PCINT16-PCINT23)
+    PCMSK1(PCINT8-PCINT14)  and PCMSK2(PCINT16-PCINT23) If at specific pin interrupt is enable that is not use for any
+    other purpose
 */
-
+///  Tn this code the PCIE2 bit is set in PCICR and PCIMSK2 is enable pin change interrupt from PD0-PD7
 #include <avr/io.h>
 #include<avr/interrupt.h>    /// include interrupt header file
 #include <util/delay.h>
@@ -12,54 +13,48 @@ in atmega 328p.there are 22 PCINTx pin .The PCICR are used to enable or disable 
 #define F_CPU  16000000UL
 #endif // F_CPU
 
-#define Int_0  (1<<PINB0)    /// PCINT0
-#define  Int_1  (1<<PINB1)    /// PCINT1
+#define Int_16  (1<<PIND0)    /// PCINT16
+#define  Int_17 (1<<PIND1)    /// PCINT7
 
-#define  led_1 (1<<PIND0)   /// OUTPUT CONTROL BY PCINT0
-#define  buzz   (1<<PIND1)    ///OUTPUT CONTROL BY PCINT1
+#define  led_1 (1<<PINB0)   /// OUTPUT CONTROL BY PCINT16
+#define  buzz   (1<<PINB1)    ///OUTPUT CONTROL BY PCINT7
 
-void TimerInit()
-{
-     TCCR0B=(1<<CS02)|(1<<CS00);   ///F_CPU/1024=64us
-  TCCR0A =(1<<WGM01);        ///making CTC mode by setting this bit
-  OCR0A=250;               ///loading reg 250x64us=16ms
-  TCNT0=0;
-}
+
 int main(void)
 {
-    DDRB &= (~Int_0)&(~Int_1);     // Clear the PB0 AND PB1pin
-    // PB0 (PCINT0 pin) is now an input
-    DDRD|=led_1|buzz;
-    PORTB |= Int_0|Int_1;    // turn On the Pull-up
-    // PB0 AND PB1is now an input with pull-up enabled
+    DDRD &= (~Int_16)&(~Int_17);     // Clear the PD0 AND PD1pin
+    // PD0 AND PD1  is now an input
+    DDRB|=led_1|buzz;
+    PORTD |= Int_16|Int_17;    // turn On the Pull-up
+    // PD0 AND PD1 is now an input with pull-up enabled
      //TimerInit();
 
-    PCICR |= (1 << PCIE0);    // set PCIE0 to enable PCMSK0 scan
-    PCMSK0 |= (1 << PCINT0)|(1<<PCINT1);  // set PCINT0 AND PCINT1 to trigger an interrupt on state change
+    PCICR |= (1 << PCIE2);    // set PCIE2 to enable PCMSK2 scan
+    PCMSK2 |= (1 << PCINT16)|(1<<PCINT17);  // set PCINT16 AND PCINT17 to trigger an interrupt on state change
     //TIMSK0=(1<<OCIE0A);
-    sei();
+    sei();   /// SET THE GLOBAL INTERRUPT ENABLE
     //sei();
     while(1);
     {
-
+   /// SIMULTANEOUSLY CAN DO MANY TASK
 
     }
     return 0;
 }
-ISR (PCINT0_vect)
+ISR (PCINT2_vect)
 {
-    if(!(PINB&Int_0))    /// ACTIVE LOW
+    if(!(PIND&Int_16))    /// ACTIVE LOW
     {
-       PORTD|=led_1;
+       PORTB|=led_1;
     }
     else
-        PORTD&=(~led_1);
-    if(!(PINB&Int_1))    ///ACTIVE LOW
+        PORTB&=(~led_1);
+    if(!(PIND&Int_17))    ///ACTIVE LOW
     {
-        PORTD|=buzz;
+        PORTB|=buzz;
     }
     else
-        PORTD&=(~buzz);
+        PORTB&=(~buzz);
 //    if((PINB&Int_1))
 //    {
 //
