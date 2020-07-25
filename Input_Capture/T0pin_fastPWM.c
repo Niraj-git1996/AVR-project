af2f13b3bab2be5cc12737fc23c08afcd259f160
Niraj-git1996 <contact.niraj1996@gmail.com>
2020-07-25 20:18:18 +0530

    the input capture mode updated code

---
+++ Input_Capture/T0pin_fastPWM.c
@@ -0,0 +1,35 @@
+/* Input capture waveform waveform generation the input capture is done by T0 pin(PD4) in atmega 328p
+*  It increments the TCNT0 value till OCR0A
+ */
+
+#include <avr/io.h>
+#define F_CPU 16000000UL;
+#include "timer0.h"
+#define button  (1<<PIND4)    /// T0 pin
+#define  led  (1<<PIND6)   ///ocoA
+
+int main(void)
+{
+  DDRD&=~button; ///input button
+   DDRD|=led;
+   OCR0A=0x00;///set the external counter value in OCR0A number of times-2
+   TCNT0=0x00;
+   //TCCR0A|=(1<<WGM01);/// CTC mode
+   TCCR0A|=(1<<WGM01)|(1<<WGM00);/// fast pwm mode
+   //TCCR0A|=set_OC0A;  ///after the counter value of external clock turn on LED connected to OC0//
+    TCCR0A|=toggle_OC0A;/// It toggles the led on OC0 pin after the certain external clock
+    TCCR0B|=T0_rising; ///clock count after rising edge
+    //  TCCR0B|=T0_falling;/// clock count after falling edge
+// Insert code
+    while(1)
+    {
+       while((TIFR0&(1<<OCF0A))==0);
+         //OCR0A=3;
+         TCNT0=0;
+        TIFR0|=(1<<OCF0A);
+
+    }
+
+
+    return 0;
+}
--- Input_Capture/timer0.h
+++ Input_Capture/timer0.h
@@ -1,6 +1,6 @@
 #ifndef TIMER0_H_INCLUDED
 #define TIMER0_H_INCLUDED
-
+/// the timer register loaded with ORing the value
  #include<avr/io.h>
 
   enum clksource{T_stop=0x00,No_prescale,clk_div8,clk_div64,clk_div256,clk_div1024,
@@ -9,5 +9,11 @@
  enum comp_modeA{Normal_portA=0x00,toggle_OC0A=0x40,clear_OC0A=0x80,set_OC0A=0xC0};///related to TCCR0A
 
  enum comp_modeB{Normal_portB=0x00,toggle_OC0B=0x10,clear_OC0B=0x20,set_OC0B=0x30};/// related to TCCR0A
+
+ enum regA_mode{mode0_A=0x00,mode1_A=0x01,mode2_A=0x02,mode3_A=0x03,mode5_A=0x01,mode7_A=0x03};/// related to TCCR0A
+
+ enum regB_mode{mode0_B=0x00,mode1_B=0x00,mode2_B=0x00,mode3_B=0x00,mode5_B=0x08,mode7_B=0x08};/// related to TCCR0B
+
+
 
 #endif // TIMER0_H_INCLUDED
